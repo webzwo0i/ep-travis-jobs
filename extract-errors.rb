b93e9a8277f297384fea4441e97a1544b57847a9
webzwo0i <webzwo0i@c3d2.de>
2020-08-09 22:33:33 +0200

    refactor

--- extract-errors.rb
+++ extract-errors.rb
@@ -3,78 +3,87 @@
 Dir.entries("jobs/").each do |filename|
   next if not File.file? "jobs/"+filename
   data = File.open("jobs/"+filename).read
+
+  #intended to skip totally broken builds
   next if not start=data.index(/Remote sauce test.*started/)
-  next if not fin=data.index(/Remote sauce test.*finished/)
+  next if not data.index(/Remote sauce test.*finished/)
+  next if data.index(/The build has been terminated/)
 
   data=data[start..-1]
 
-  errors = []
+  @errors = []
   state = "skip"
-  error = ""
+  @error = ""
   indent = 0
 
+  def push_error
+    if @error != ""
+      @errors.push @error
+      @error = ""
+    end
+  end
+
   data.split("\n").each do |line|
-    next if line.match(/Remote sauce test/)
 
-    if line.match(/FINISHED|^$/)
-      if error != ""
-        errors.push error
-        error = ""
-      end
+    if line.match(/Remote sauce test|FINISHED|^\s+?$|PASSED|PENDING/)
+      push_error
       state = "skip"
       next
     end
 
-    if not line.match(/PENDING|PASSED|FAILED/) and (state == "skip")
+    # multiline output of a non-failing test
+    if not line.match(/PENDING|PASSED|FAILED/) and state == "skip"
       next
     end
 
-    if line.match(/PASSED|PENDING/)
-      if error != ""
-        errors.push error
-        error = ""
-      end
-      state = "skip"
-      next
-    end
 
+    # a failed test
     if line.match(/FAILED/)
-      if error != ""
-        errors.push error
-        error = ""
-      end
+      push_error
+      # save the indention level to detect multiline test descriptions
       if m=line.match(/^\[[^\]]+\](\s+)/)
         indent = m[1].size
       end
       line.gsub!(/^\[[^\]]+\]\s+-> FAILED : /,"")
       state = "fail"
-      error = line
+      @error = line
       next
     end
 
+    #if line.match(/allowed test duration exceeded/)
+    #  if error != ""
+    #    errors.push error
+    #    error = line.strip
+    #  end
+    #  state = "skip"
+    #  next
+    #end
+
     if state == "fail"
+      # a new suite
       if m=line.match(/^\[[^\]]+\](\s+)/) and m[1].size < indent
         state = "skip"
         indent = 0
         next
       end
+      # multiline test description
       line.gsub!(/^\[[^\]]+\]\s+/,"")
-      error += line.strip
-      next
+      @error += line.strip
     end
-
   end
 
-  errors.map! do |e|
+
+  # skip known errors
+  @errors.map! do |e|
     e if not e.match(/Pad never loaded|Padnever loaded|Pad neverloaded/) # a space is somewhere lost in this script
-  end
+  end.compact!
 
   # skip if more than 10 errors are in a file
-  next if errors.size > 10
+  #next if @errors.size > 10
 
-  if errors.size > 0
+  if @errors.size > 0
     File.open("failed/#{filename}","w") do |f|
-      f.write errors.join("\n")
+      f.write @errors.join("\n")
       f.write "\n"
     end
   end
