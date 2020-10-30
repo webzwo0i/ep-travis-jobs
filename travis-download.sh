9b1f39c6525c7722dd68058eca3efe21448bd753
webzwo0i <webzwo0i@c3d2.de>
2020-10-30 13:08:45 +0100

    update and move to travis-ci.com

--- travis-download.sh
+++ travis-download.sh
@@ -6,16 +6,16 @@ repo=ether/etherpad-lite
 #TODO: find out if it is possible to only download new builds
 #get all build number
 echo "getting build history"
-$travis history --repo ether/etherpad-lite --limit 20000 >build-history
+$travis history --com --repo ether/etherpad-lite --limit 20000 >build-history
 
 echo "download builds"
 #only download new builds
 for buildid in $(((ls -1 builds/) && (awk '{gsub(/#/,"");print $1}' build-history|sort|uniq))|sort|uniq -u);do
-  $travis show --repo $repo $buildid >builds/$buildid
+  $travis show --com --repo $repo $buildid >builds/$buildid
 done
 
 echo "download jobs"
 #only download new jobs
 for jobid in $(((ls -1 jobs/) && (grep -Pr "(?<=^Job #)\d+\.\d+|(?<=^#)\d+\.\d+" builds -oh))|sort|uniq -u);do
-  $travis logs --repo $repo $jobid > jobs/$jobid
+  $travis logs --com --repo $repo $jobid > jobs/$jobid
 done
